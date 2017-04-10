b4a0e94d5230751acb23b21166e854e771ec17b3
Armon ROHANI <arohani@e2r7p3.42.fr>
2017-04-10 19:42:09 +0200

    adds validated ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arohani <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/10 19:39:39 by arohani           #+#    #+#             */
+/*   Updated: 2017/04/10 19:41:54 by arohani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		j;
+	char	*dest;
+
+	i = 0;
+	if (!src)
+		return (NULL);
+	while (src[i])
+		i++;
+	dest = malloc(sizeof(*dest) * (i + 1));
+	j = 0;
+	if (!dest)
+		return (NULL);
+	while (j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
