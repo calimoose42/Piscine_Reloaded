ffc820d61c03fbc1dc8baed4c28821ecb56827d1
Armon ROHANI <arohani@e1r2p7.42.fr>
2017-04-12 13:10:44 +0200

    fixes typo in ex26

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: arohani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 12:52:08 by arohani           #+#    #+#             */
-/*   Updated: 2017/04/12 12:56:56 by arohani          ###   ########.fr       */
+/*   Updated: 2017/04/12 13:07:54 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_count_if(char **tab, int (*f)(char*))
 	result = 0;
 	while (tab[i][0] != '0')
 	{
-		if (f(tab[i] == 1))
+		if (f(tab[i]) == 1)
 			result++;
 		i++;
 	}
